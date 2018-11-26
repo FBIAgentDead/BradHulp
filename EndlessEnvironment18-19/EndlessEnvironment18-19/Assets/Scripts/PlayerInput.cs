0acb97cd9dc49e2f29d43bbc0322987291f43851
Brettboy2001 <39295932+Brettboy2001@users.noreply.github.com>
2018-11-26 09:55:41 +0100

    ok dan

--- EndlessEnvironment18-19/EndlessEnvironment18-19/Assets/Scripts/PlayerInput.cs
+++ EndlessEnvironment18-19/EndlessEnvironment18-19/Assets/Scripts/PlayerInput.cs
@@ -1,50 +1,88 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (Player))]
-public class PlayerInput : MonoBehaviour {
+[RequireComponent(typeof(Player))]
+public class PlayerInput : MonoBehaviour
+{
 
-	Player player;
-	Animator playAnim;
-	SpriteRenderer flipPlayer;
-	bool leftOrRight;
+    Player player;
 
-	void Start () {
-		player = GetComponent<Player> ();
-		playAnim = GetComponent<Animator>();
-		flipPlayer = GetComponent<SpriteRenderer>();
-	}
+    bool negativeGravity = false;
+    [SerializeField]
+    LayerMask mask;
+    [SerializeField]
+    private float checkHeight;
+    RaycastHit2D hit;
+    Animator playAnim;
+    SpriteRenderer flipPlayer;
+    bool leftOrRight;
 
-	void Update () {
-		Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
-		player.SetDirectionalInput (directionalInput);
+    void Start()
+    {
+        player = GetComponent<Player>();
+        playAnim = GetComponent<Animator>();
+        flipPlayer = GetComponent<SpriteRenderer>();
+    }
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			player.OnJumpInputDown ();
-            playAnim.Play("Jump");
-		}
-		if (Input.GetKey(KeyCode.Space)) {
-			player.OnJumpInputUp ();
-            playAnim.Play("Jump");
-		}
-	    else if(Input.GetKey(KeyCode.LeftArrow)){
-			playAnim.Play("walk");
-			if(leftOrRight == true){
-				flipPlayer.flipX = true;
-                leftOrRight = false;
-			}
-		}
-        else if (Input.GetKey(KeyCode.RightArrow))
+    bool checkGrounded()
+    {
+        //this should make sense it checks if the player is grounded with the use of a raycast. it checks up if player gravity is negative 
+        //and down if player has positive gravity
+        if (negativeGravity == false)
         {
-			playAnim.Play("walk");
-            if (leftOrRight == false)
+            hit = Physics2D.Raycast(transform.position, -Vector2.up, checkHeight, mask);
+        }
+        else if (negativeGravity == true)
+        {
+            hit = Physics2D.Raycast(transform.position, Vector2.up, checkHeight, mask);
+        }
+
+        return hit.collider == null ? false : true;
+    }
+
+    void Update()
+    {
+        Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        player.SetDirectionalInput(directionalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.OnJumpInputDown();
+            playAnim.Play("Jump");
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            player.OnJumpInputUp();
+            playAnim.Play("Jump");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (checkGrounded())
             {
-                flipPlayer.flipX = false;
-				leftOrRight = true;
+                playAnim.Play("walk");
+                if (leftOrRight == true)
+                {
+                    flipPlayer.flipX = true;
+                    leftOrRight = false;
+                }
             }
         }
-		else{
-			playAnim.Play("idle");
-		}
-	}
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (checkGrounded())
+            {
+                playAnim.Play("walk");
+                if (leftOrRight == false)
+                {
+                    flipPlayer.flipX = false;
+                    leftOrRight = true;
+                }
+            }
+        }
+        else
+        {
+            if (checkGrounded())
+                playAnim.Play("idle");
+        }
+    }
 }
